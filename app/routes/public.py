e7602da79579f10deadc80ed092582c46d6df7ee
RamdotzX <147622188+SatriaParama@users.noreply.github.com>
2026-01-26 16:33:52 +0700

    Add session import to public.py

--- app/routes/public.py
+++ app/routes/public.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from datetime import datetime
 from app.extensions import db
 from app.models import Film, Schedule, Booking, User
